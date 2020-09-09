8d828c4b999c0dc60f7a7d5f5dba37dd4931f159
jhonatan-dev <jhonatancastillosalvador@hotmail.com>
2020-09-08 22:09:35 -0500

    dasdsasdadsd

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
+  
     host: "peerjsservidor.herokuapp.com",
     port: 443,
     secure: true,
