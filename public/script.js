42c68f788e0215476f08b9ac6ef3b9f7a9a7e301
jhonatan-dev <jhonatancastillosalvador@hotmail.com>
2020-09-10 12:03:55 -0500

    https://github.com/CleverProgrammers/nodejs-zoom-clone

--- public/script.js
+++ public/script.js
@@ -1,77 +1,20 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-  
-    host: "peerjsservidor.herokuapp.com",
-    port: 443,
-    secure: true,
-    debug: 3,
-    config: {
-      iceServers: [
-        {
-          urls: "stun:3.15.4.15?transport=udp",
-          username: "ruedaapp",
-          credential: "ruedaapp"
-        },
-        {
-          urls: "stun:3.15.4.15?transport=tcp",
-          username: "ruedaapp",
-          credential: "ruedaapp"
-        },
-        {
-          urls: "turn:3.15.4.15?transport=udp",
-          username: "ruedaapp",
-          credential: "ruedaapp"
-        },
-        {
-          urls: "stun:3.15.4.15?transport=tcp",
-          username: "ruedaapp",
-          credential: "ruedaapp"
-        }
-
-        /*
-        { urls: "stun:stun.l.google.com:19302" },
-        { urls: "stun:stun1.l.google.com:19302" },
-        { urls: "stun:stun2.l.google.com:19302" },
-        { urls: "stun:stun3.l.google.com:19302" },
-        { urls: "stun:stun4.l.google.com:19302" },
-        {
-          urls: "turn:numb.viagenie.ca?transport=tcp",
-          username: "datay32672@acetesz.com",
-          credential: "123456"
-        },
-        {
-          urls: "turn:numb.viagenie.ca?transport=udp",
-          username: "datay32672@acetesz.com",
-          credential: "123456"
-        },
-        {
-          urls: [ "stun:us-turn9.xirsys.com" ]
-       },
-        {
-          username: "DVg5ibvyLXZ_vgYfwrr9g9z1Q3iDCcxLO--eYA7caTvOQCxK2gY6EXWfxEpghr8CAAAAAF9X9uJydWVkYWFwcA==",
-          credential: "e0b6fc8c-f219-11ea-8e84-0242ac140004",
-          urls: [
-              "turn:us-turn9.xirsys.com:80?transport=udp",
-              "turn:us-turn9.xirsys.com:3478?transport=udp",
-              "turn:us-turn9.xirsys.com:80?transport=tcp",
-              "turn:us-turn9.xirsys.com:3478?transport=tcp",
-              "turns:us-turn9.xirsys.com:443?transport=tcp",
-              "turns:us-turn9.xirsys.com:5349?transport=tcp"
-          ]
-       }*/
-      ]
-    }
+  path: '/peerjs',
+  host: '/',
+  port: '443'
 })
+let myVideoStream;
 const myVideo = document.createElement('video')
-myVideo.muted = true
+myVideo.muted = true;
 const peers = {}
 navigator.mediaDevices.getUserMedia({
   video: true,
   audio: true
 }).then(stream => {
+  myVideoStream = stream;
   addVideoStream(myVideo, stream)
-
   myPeer.on('call', call => {
     call.answer(stream)
     const video = document.createElement('video')
@@ -82,6 +25,19 @@ navigator.mediaDevices.getUserMedia({
 
   socket.on('user-connected', userId => {
     connectToNewUser(userId, stream)
+  })
+  // input value
+  let text = $("input");
+  // when press enter send message
+  $('html').keydown(function (e) {
+    if (e.which == 13 && text.val().length !== 0) {
+      socket.emit('message', text.val());
+      text.val('')
+    }
+  });
+  socket.on("createMessage", message => {
+    $("ul").append(`<li class="message"><b>user</b><br/>${message}</li>`);
+    scrollToBottom()
   })
 })
 
@@ -112,4 +68,67 @@ function addVideoStream(video, stream) {
     video.play()
   })
   videoGrid.append(video)
+}
+
+
+
+const scrollToBottom = () => {
+  var d = $('.main__chat_window');
+  d.scrollTop(d.prop("scrollHeight"));
+}
+
+
+const muteUnmute = () => {
+  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    setUnmuteButton();
+  } else {
+    setMuteButton();
+    myVideoStream.getAudioTracks()[0].enabled = true;
+  }
+}
+
+const playStop = () => {
+  console.log('object')
+  let enabled = myVideoStream.getVideoTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getVideoTracks()[0].enabled = false;
+    setPlayVideo()
+  } else {
+    setStopVideo()
+    myVideoStream.getVideoTracks()[0].enabled = true;
+  }
+}
+
+const setMuteButton = () => {
+  const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>
+  `
+  document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setUnmuteButton = () => {
+  const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+  `
+  document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setStopVideo = () => {
+  const html = `
+    <i class="fas fa-video"></i>
+    <span>Stop Video</span>
+  `
+  document.querySelector('.main__video_button').innerHTML = html;
+}
+
+const setPlayVideo = () => {
+  const html = `
+  <i class="stop fas fa-video-slash"></i>
+    <span>Play Video</span>
+  `
+  document.querySelector('.main__video_button').innerHTML = html;
 }
